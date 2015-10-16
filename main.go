1405e36343093fa45de9ab8c116a47a783cf302a
Phil Frost <phil.frost@paperg.com>
2015-10-16 08:11:30 -0400

    Consolidate into one package and configure CI
    This layout is simpler and allows CircleCI to work without any explicit
    configuration.

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-
-	"github.com/paperg/terraform-etcd/etcd"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: etcd.Provider,
+		ProviderFunc: Provider,
 	})
 }
--- provider.go
+++ provider.go
@@ -1,4 +1,4 @@
-package etcd
+package main
 
 import (
 	"github.com/coreos/go-etcd/etcd"
